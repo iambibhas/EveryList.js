51e3b267f33df8290d6e10cd389c24bd1587adba
Bibhas C Debnath <iambibhas@gmail.com>
2012-10-21 14:39:44 +0530

    trying to getJSON from github

--- everylist.js
+++ everylist.js
@@ -19,24 +19,31 @@
     EveryList.prototype.init = function () {
         this.options.tag = this.element.tagName;
         if(!this.options.hasOwnProperty('listName')){
-            return this.errorOnMissingParameter('listName');
+            return this.errorOnMissingParameter('init', 'listName');
         }
-        list = this.getList(this.options.listName);
-        console.log(list);
+        this.getList(this.options.listName);
     };
 
     EveryList.prototype.getList = function(name){
         if(name == 'COUNTRY'){
             // returning dummy list
-            return {};
+            this.readFile(name);
         }
     };
 
     EveryList.prototype.readFile = function(fileName){
+        if(fileName == undefined || fileName == ""){
+            return this.errorOnMissingParameter('readFile', 'fileName');
+        }
 
+        var listurl = "http://iambibhas.github.com/EveryList.js/lists/" + fileName + "?callback=?"
+        $.getJSON(listurl, function(data) {
+            console.log(pluginName + ': List found - ' + fileName);
+            console.log(data);
+        });
     }
 
-    EveryList.prototype.errorOnMissingParameter = function(paramName){
+    EveryList.prototype.errorOnMissingParameter = function(funcName, paramName){
         if(paramName != undefined){
             console.log(pluginName + ': Must provide property - ' + paramName);
             return false;
